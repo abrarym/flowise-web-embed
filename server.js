177ed493a02b20a17801709829fc107d7a1fa076
abrarym <mabrary@my.bcit.ca>
2023-10-18 13:47:08 -0700

    change server

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 3000;
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on ${PORT}`);
 });
